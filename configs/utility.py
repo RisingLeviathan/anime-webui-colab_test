695c49da17ba4297a2ad2d89f1902b0dac7120b7
RisingLeviathan <146772095+RisingLeviathan@users.noreply.github.com>
2025-09-01 15:00:18 -0300

    Update utility.py

--- configs/utility.py
+++ configs/utility.py
@@ -260,8 +260,8 @@ def patch_list():
     'pip install -q httpx',
     'pip uninstall tokenizers',
     'pip uninstall transformers',
-    'pip install jedi',
     'pip install --upgrade pip setuptools wheel',
+    'pip install jedi',
     'pip install transformers --extra-index-url https://download.pytorch.org/whl/cu121',
     'pip install tokenizers --extra-index-url https://pypi.org/simple',
     'pip uninstall torch torchvision torchaudio xformers -y',
@@ -537,6 +537,7 @@ notices = [
 for notice in notices:
 
   print(notice)
+
 
 
 
